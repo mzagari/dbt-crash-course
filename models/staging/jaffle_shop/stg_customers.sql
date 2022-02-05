3dbd6ff82e697b1ea1dbb8af2422aa5d1ea5a1ae
Martin J Zagari <drzagari@outlook.com>
2022-02-05 05:08:42 +0000

    First commit from fundamentals course

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,7 @@
+SELECT
+    id as customer_id,
+    first_name,
+    last_name
+
+FROM {{ source('jaffle_shop', 'customers') }}
+--FROM jaffle.customers
---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,8 @@
+SELECT
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+FROM {{ source('jaffle_shop', 'orders') }}
+--FROM jaffle.orders
